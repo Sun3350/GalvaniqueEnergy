bc7b7fac4945f049688602aeb612d25051d09c82
Ayomide <isaacayomide2359@gmail.com>
2025-09-02 10:08:46 +0100

    update

--- galvaniqueenergy/src/app/layout.tsx
+++ galvaniqueenergy/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <Header />
           <main className="min-h-screen">{children}</main>
